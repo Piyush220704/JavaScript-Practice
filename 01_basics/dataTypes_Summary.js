a998d56366b1ccde0a8937644c53dd5d0bc4e312
Piyush Arora <122970722+Piyush220704@users.noreply.github.com>
2024-10-02 10:21:22 +0000

    dataTypes summery

---
+++ 01_basics/dataTypes_Summary.js
@@ -0,0 +1,25 @@
+//primitive & non primitive/reference type
+/*
+the comparison is made on the basis of how the data is stored in memory
+
+Primitive types ->
+String
+Number
+Boolean
+null
+undefined
+Symbol
+BigInt
+
+Reference types ->
+arrays
+objects
+functions
+
+javascript is a dynamically typed language
+*/
+
+//symbol example
+// const id = Symbol("123");
+// const userID = Symbol("123");
+// console.log(id === userID); if we don't use Symbol then it'll give true 
